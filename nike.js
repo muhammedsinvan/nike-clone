9914c4165eccd9c23f3a8841f2ed1d7807e8e4f1
muhammedsinvan <92931932+muhammedsinvan@users.noreply.github.com>
2021-10-30 10:15:01 +0530

    Add files via upload

---
+++ nike.js
@@ -0,0 +1,13 @@
+
+   var demo = document.getElementById("hide-item");
+
+  document.getElementById("mobile-show").addEventListener("mouseenter" , ()=>{
+   demo.style.display = "block";
+  })
+
+  demo.addEventListener("mouseleave" , ()=>{
+    demo.style.display = "none";
+  })
+
+
+ 
